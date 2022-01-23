7b1d62eabe381ac5f1fa627a4757b5142d724d58
Bruce Wright <bjw.heltor@gmail.com>
2022-01-23 21:02:43 +0000

    Added type hints - 7

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -34,6 +34,9 @@ release = "0.1"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx_autodoc_typehints",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -56,3 +59,10 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {
+    "https://docs.python.org/": None,
+    "https://docs.scipy.org/doc/numpy/": None,
+    "https://www.pygame.org/docs/": None,
+}
